fdd2e4ffdb545ab046eca0d126583e6ef315828c
Umar Ali <edumail.umarali@gmail.com>
2025-12-11 03:33:41 +0000

    Final Commit

---
+++ jenkins-deployment/data.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "main" {
+  owners   = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20251015"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_availability_zones" "azs" {}
